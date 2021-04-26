5a57a861059f080e341ed6a2b99229a3d715ad11
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-04-25 22:11:35 -0400

    updated date, and added updated comments

--- Leapyear.py
+++ Leapyear.py
@@ -1,7 +1,8 @@
 #Jackson E. Rollins
-#4/11/2021
+#4/25/2021
 
-#Program that asks for a year from the user and then determines if it is a leap year or not
+#Program that asks for a year from the user and then determines if it is a leap year or not.
+#Repatedly asks the user for a new number if the number given is not a positive integer
 
 n = input("Please enter a year. Positive integer values only: ")
 
